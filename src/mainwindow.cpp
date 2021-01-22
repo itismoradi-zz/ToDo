663d91446578b281266fd1b36a54e37692fad5a9
Amir Ghazi <ghaziamir844@gmail.com>
2021-01-22 08:08:41 +0800

    por kardan combobox

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -6,6 +6,10 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    ui->comboBox->addItem("No Time");
+    ui->comboBox->addItem("Mon");
+    ui->comboBox->addItem("Week");
+    ui->comboBox->addItem("Day");
 }
 
 MainWindow::~MainWindow()
